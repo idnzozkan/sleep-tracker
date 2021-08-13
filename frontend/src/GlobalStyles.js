d5889b4d803730aab857ebcb01c8c72fcaed36b8
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-08-13 23:55:13 +0300

    Add styles for custom scrollbar

--- frontend/src/GlobalStyles.js
+++ frontend/src/GlobalStyles.js
@@ -1,6 +1,21 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
+    html {
+        &::-webkit-scrollbar {
+            width: 5px;
+            height: 5px;
+        }
+        &::-webkit-scrollbar-track {
+            background-color: rgba(255, 255, 255, 0.1);
+            border-radius: 10px;
+        }
+        &::-webkit-scrollbar-thumb {
+            background-color: #465EAD;
+            border-radius: 10px;
+        }
+    }
+
     body {
         background: #2A2A42;
     }
