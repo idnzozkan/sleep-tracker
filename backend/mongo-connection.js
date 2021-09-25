68e896bdc4d650b08efd4f485a8dd60bae4cadda
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-09-25 23:19:34 +0300

    Set an ENV variable for MongoDB Connection String

--- backend/mongo-connection.js
+++ backend/mongo-connection.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/sleep-tracker", {
+const connectionString = process.env.MONGODB_CONNECTION_STRING || "mongodb://database/sleep-tracker"
+
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
